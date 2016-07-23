65fe556407bdd80d8174e21d84717949d5a2f640
da_ta_vinci <akashtndn.acm@gmail.com>
2016-07-23 15:08:31 +0530

    Used Applicant Birthdate info to create Applicant Age variable

--- model_building.R
+++ model_building.R
@@ -2,6 +2,7 @@ library(xgboost)
 library(Matrix)
 library(caret)
 library(ROCR)
+library(tidyr)
 
 set.seed(1234)
 
@@ -13,10 +14,66 @@ train.id <- train$ID
 train.y <- train$Business_Sourced
 
 ## FEATURE ENGINEERING
-train <- train[,c(5, 7:9, 11:15, 17:23)]
-test <- test[,c(5, 7:9, 11:15, 17:22)]
+train <- train[,c(3, 5, 6:9, 11:15, 17:23)]
+test <- test[,c(3, 5, 6:9, 11:15, 17:22)]
+
+train$Business_Sourced <- NULL
+
+# Separating Application_Receipt_Date into date, month, year
+# ----------------------------------------------------------
+train <- separate(data = train, col = Application_Receipt_Date, 
+         into = c("Receipt_month", "Receipt_date", "Receipt_year"))
+train$Receipt_date <- as.numeric(train$Receipt_date)
+train$Receipt_month <- as.numeric(train$Receipt_month)
+train$Receipt_year <- as.numeric(train$Receipt_year)
+
+test <- separate(data = test, col = Application_Receipt_Date, 
+                  into = c("Receipt_month", "Receipt_date", "Receipt_year"))
+test$Receipt_date <- as.numeric(test$Receipt_date)
+test$Receipt_month <- as.numeric(test$Receipt_month)
+test$Receipt_year <- as.numeric(test$Receipt_year)
+
+# Separating Applicant_BirthDate into date, month, year
+# ----------------------------------------------------------
+train <- separate(data = train, col = Applicant_BirthDate, 
+                  into = c("Applicant_Birth_month", "Applicant_Birth_date", "Applicant_Birth_year"))
+# train$Applicant_Birth_date <- as.numeric(train$Applicant_Birth_date)
+# train$Applicant_Birth_date[is.na(train$Applicant_Birth_date)] <- 
+#     as.numeric(names(which.max((table(train$Applicant_Birth_date)))))
+# 
+# train$Applicant_Birth_month <- as.numeric(train$Applicant_Birth_month)
+# train$Applicant_Birth_month[is.na(train$Applicant_Birth_month)] <- 
+#     as.numeric(names(which.max((table(train$Applicant_Birth_month)))))
+train$Applicant_Birth_date <- NULL
+train$Applicant_Birth_month <- NULL
+train$Applicant_Birth_year <- as.numeric(train$Applicant_Birth_year)
+train$Applicant_Birth_year[is.na(train$Applicant_Birth_year)] <- 
+    as.numeric(names(which.max((table(train$Applicant_Birth_year)))))
+
+test <- separate(data = test, col = Applicant_BirthDate, 
+                 into = c("Applicant_Birth_month", "Applicant_Birth_date", "Applicant_Birth_year"))
+# test$Applicant_Birth_date <- as.numeric(test$Applicant_Birth_date)
+# test$Applicant_Birth_date[is.na(test$Applicant_Birth_date)] <- 
+#     as.numeric(names(which.max((table(test$Applicant_Birth_date)))))
+# 
+# test$Applicant_Birth_month <- as.numeric(test$Applicant_Birth_month)
+# test$Applicant_Birth_month[is.na(test$Applicant_Birth_month)] <- 
+#     as.numeric(names(which.max((table(test$Applicant_Birth_month)))))
+test$Applicant_Birth_date <- NULL
+test$Applicant_Birth_month <- NULL
+test$Applicant_Birth_year <- as.numeric(test$Applicant_Birth_year)
+test$Applicant_Birth_year[is.na(test$Applicant_Birth_year)] <- 
+    as.numeric(names(which.max((table(test$Applicant_Birth_year)))))
+
+# Create Age variable
+# ---------------------
+train$Age <- (2015 - train$Applicant_Birth_year)
+train$Applicant_Birth_year <- NULL
+test$Age <- (2015 - test$Applicant_Birth_year)
+test$Applicant_Birth_year <- NULL
 
 # Encoding Applicant_Gender
+# ----------------------------------------------------------
 train$Applicant_Gender[train$Applicant_Gender == "F"] <- 1
 train$Applicant_Gender[train$Applicant_Gender == "M"] <- 2
 train$Applicant_Gender[train$Applicant_Gender == ""] <- 3
@@ -36,6 +93,7 @@ test$Applicant_Gender <- as.numeric(test$Applicant_Gender)
 # test$Applicant_Gender <- NULL
 
 # Encoding Applicant_Marital_Status
+# ----------------------------------------------------------
 train$Applicant_Marital_Status[train$Applicant_Marital_Status == "M"] <- 1
 train$Applicant_Marital_Status[train$Applicant_Marital_Status == "S"] <- 2
 train$Applicant_Marital_Status[train$Applicant_Marital_Status == "D"] <- 3
@@ -53,6 +111,7 @@ test$Applicant_Marital_Status[test$Applicant_Marital_Status == "W" |
 test$Applicant_Marital_Status <- as.numeric(test$Applicant_Marital_Status)
 
 # Encoding Applicant_Occupation
+# ----------------------------------------------------------
 train$Applicant_Occupation[train$Applicant_Occupation == "Salaried"] <- 1
 train$Applicant_Occupation[train$Applicant_Occupation == "Business"] <- 2
 train$Applicant_Occupation[train$Applicant_Occupation == "Others" |
@@ -70,6 +129,7 @@ test$Applicant_Occupation[test$Applicant_Occupation == "Self Employed" |
 test$Applicant_Occupation <- as.numeric(test$Applicant_Occupation)
 
 # Encoding Applicant_Qualification
+# ----------------------------------------------------------
 train$Applicant_Qualification[train$Applicant_Qualification == "Class XII"] <- 1
 train$Applicant_Qualification[train$Applicant_Qualification == "Graduate"] <- 2
 train$Applicant_Qualification[train$Applicant_Qualification == "Class X"] <- 3
@@ -87,6 +147,7 @@ test$Applicant_Qualification[test$Applicant_Qualification != 1 &
 test$Applicant_Qualification <- as.numeric(test$Applicant_Qualification)
 
 # Encoding Manager_Joining_Designation
+# ----------------------------------------------------------
 temp_joining_des <- train$Manager_Joining_Designation
 train$Manager_Joining_Designation <- 6
 train$Manager_Joining_Designation[temp_joining_des == "Level 1"] <- 1
@@ -110,6 +171,7 @@ test$Manager_Joining_Designation[temp_joining_des == "Level 5" |
 rm(temp_joining_des)
 
 # Encoding Manager_Current_Designation
+# ----------------------------------------------------------
 temp_current_des <- train$Manager_Current_Designation
 train$Manager_Current_Designation <- 6
 train$Manager_Current_Designation[temp_current_des == "Level 1"] <- 1
@@ -133,6 +195,7 @@ test$Manager_Current_Designation[temp_current_des == "Level 5" |
 rm(temp_current_des)
 
 # Encoding Manager_Status
+# ----------------------------------------------------------
 train$Manager_Status[train$Manager_Status == "Confirmation"] <- 1
 train$Manager_Status[train$Manager_Status == "Probation"] <- 2
 train$Manager_Status[train$Manager_Status == ""] <- 3
@@ -144,6 +207,7 @@ test$Manager_Status[test$Manager_Status == ""] <- 3
 test$Manager_Status <- as.numeric(test$Manager_Status)
 
 # Encoding Manager_Gender
+# ----------------------------------------------------------
 train$Manager_Gender[train$Manager_Gender == "F"] <- 1
 train$Manager_Gender[train$Manager_Gender == "M"] <- 2
 train$Manager_Gender[train$Manager_Gender == ""] <- 3
@@ -155,6 +219,7 @@ test$Manager_Gender[test$Manager_Gender == ""] <- 3
 test$Manager_Gender <- as.numeric(test$Manager_Gender)
 
 # Imputing the numeric variables
+# ----------------------------------------------------------
 train$Manager_Grade[!complete.cases(train)] <- median(train$Manager_Grade, na.rm = T)
 train$Manager_Num_Application[!complete.cases(train)] <- 2.00
 train$Manager_Num_Coded[!complete.cases(train)] <- mean(train$Manager_Num_Coded, na.rm = T)
@@ -179,6 +244,9 @@ test$Manager_Num_Products2[!complete.cases(test)] <- median(test$Manager_Num_Pro
 
 ######################
 
+## Add back the Target variable, Business_Sourced
+train$Business_Sourced <- train.y
+
 ## BUILD MODEL WITH STRATIFIED K-FOLD CV
 folds <- createFolds(as.factor(train$Business_Sourced), k = 5)
 fold_auc <- c()
@@ -189,26 +257,26 @@ for (fold in folds) {
     x_test <- train[fold, ]
     x_test.y <- train$Business_Sourced[fold]
     
-    x_train <- sparse.model.matrix(Business_Sourced ~ .-1, data= x_train) 
-    x_test <- sparse.model.matrix(Business_Sourced ~ .-1, data = x_test)
+    x_train <- sparse.model.matrix(Business_Sourced ~ ., data= x_train) 
+    x_test <- sparse.model.matrix(Business_Sourced ~ ., data = x_test)
     
     d_train <- xgb.DMatrix(data = x_train, label = x_train.y)
     d_test <- xgb.DMatrix(data = x_test, label = x_test.y)
     watchlist <- list(train=d_train, test=d_test)
     
+    # Changed depth from 2 to 4
     param <- list( objective    = "binary:logistic",
                    booster      = "gbtree",
                    eval_metric  = "auc",
                    eta          = 0.05,
-                   max_depth    = 2
+                   max_depth    = 4
                    )
     
     clf <- xgb.train(   params              = param, 
                         data                = d_train, 
                         nrounds             = 125, 
                         verbose             = 2,
-                        watchlist           = watchlist,
-                        maximize            = FALSE
+                        watchlist           = watchlist
                         )
     
     fold_pred <- predict(clf, x_test)
@@ -216,7 +284,6 @@ for (fold in folds) {
     auc <- performance(pred, measure = "auc")
     fold_auc <- c(fold_auc, auc@y.values[[1]])
 }
-
 
 print(fold_auc)
 ########################################
@@ -229,7 +296,7 @@ param <- list( objective    = "binary:logistic",
                booster      = "gbtree",
                eval_metric  = "auc",
                eta          = 0.05,
-               max_depth    = 8
+               max_depth    = 4
 )
 
 clf <- xgb.train(   params              = param, 
@@ -246,5 +313,5 @@ test <- sparse.model.matrix(target ~ ., data = test)
 preds <- predict(clf, test)
 submission <- data.frame(ID=test.id, Business_Sourced=preds)
 cat("saving the submission file\n")
-write.csv(submission, "Submissions/submission5.csv", row.names = F)
+write.csv(submission, "Submissions/submission7.csv", row.names = F)
 
